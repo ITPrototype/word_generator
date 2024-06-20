ca9f8f987b3f4810ad411a36ddb76e23b439c24e
Kamron Mamedov <94013172+ITPrototype@users.noreply.github.com>
2024-06-20 15:53:28 +0500

    v 0.1

---
+++ random_word_generator.py
@@ -0,0 +1,26 @@
+import random
+
+words = input("Enter key words:").split(" ")
+
+def random_generator(size,words):
+    return ''.join(random.choice(words) for _ in range(size))
+
+lower_words = [word.lower() for word in words]
+upper_words = [word.upper() for word in words]
+capitalized_words = [word.capitalize() for word in words]
+random_words = []
+
+for x in range(15):
+    random_words.append(random_generator(size=2,words=words))
+    random_words.append(random_generator(size=2,words=lower_words))
+    random_words.append(random_generator(size=2,words=upper_words))
+    random_words.append(random_generator(size=2,words=capitalized_words))
+
+
+
+united_words = lower_words+upper_words+capitalized_words+random_words+words
+with open("wlist.txt","w") as f:
+    for word in united_words:
+        f.write(f"{word}\n")
+
+print("Saved to wlist.txt")
